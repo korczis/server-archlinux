dcffe35ac1b5a3620be6d51e9572de6e4b96a710
Tomas Korcak <korczis@gmail.com>
2019-03-14 06:11:05 +0100

    Refactored fs role

---
+++ ansible/roles/fs/templates/mnt/root/grub-install.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env sh
+
+mkinitcpio -p linux
+
+grub-install --recheck --target=x86_64-efi --bootloader-id=GRUB --debug --modules="part_gpt part_msdos mdraid09 mdraid1x" --removable --efi-directory=/boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
