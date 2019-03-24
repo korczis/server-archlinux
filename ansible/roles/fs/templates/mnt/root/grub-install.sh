496f7f5ae44f91e5b1b6d1281ff0da1f97ceed5f
Tomas Korcak <korczis@gmail.com>
2019-03-24 16:42:53 +0100

    Use 'BOOT' as bootlader-id instead of 'GRUB'

--- ansible/roles/fs/templates/mnt/root/grub-install.sh
+++ ansible/roles/fs/templates/mnt/root/grub-install.sh
@@ -2,6 +2,6 @@
 
 mkinitcpio -p linux
 
-grub-install --recheck --target=x86_64-efi --bootloader-id=GRUB --debug --modules="part_gpt part_msdos mdraid09 mdraid1x" --removable --efi-directory=/boot/efi
+grub-install --recheck --target=x86_64-efi --bootloader-id=BOOT --debug --modules="part_gpt part_msdos mdraid09 mdraid1x" --removable --efi-directory=/boot/efi
 
 grub-mkconfig -o /boot/grub/grub.cfg
